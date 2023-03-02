a05de60ed8b6b71641286bc3be90de00982f7b2d
DAM2Alu12 <DAM2Alu12@B01-PC12.IESGUARA.LOCAL>
2023-03-02 10:26:05 +0100

    Se crea la clase Arma y se mejora el proyecto de Alberto.

---
+++ GuiaWeb/src/main/java/com/web/GuiaWeb/GuiaWebApplication.java
@@ -0,0 +1,13 @@
+package com.web.GuiaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaWebApplication.class, args);
+	}
+
+}
---
+++ GuiaWeb/src/main/java/com/web/GuiaWeb/models/dao/IArmaDao.java
@@ -0,0 +1,16 @@
+package com.web.GuiaWeb.models.dao;
+
+import java.util.List;
+
+import com.web.GuiaWeb.models.entity.Arma;
+
+public interface IArmaDao {
+	public List<Arma> findAll();
+
+	//public void save(Cliente cliente);
+
+	//public Cliente findOne(Long id);
+
+	//public void delete(Long id);
+
+}
