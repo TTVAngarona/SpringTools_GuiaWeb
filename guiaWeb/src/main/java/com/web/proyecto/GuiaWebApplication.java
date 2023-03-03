2474fc64cdc79ab29cf611f3509564a5db90bacb
DAM2Alu12 <DAM2Alu12@B01-PC12.IESGUARA.LOCAL>
2023-03-03 13:57:01 +0100

    Añadimos el nuevo proyecto...

--- guiaWeb/src/main/java/com/web/proyecto/GuiaWebApplication.java
+++ guiaWeb/src/main/java/com/web/proyecto/GuiaWebApplication.java
@@ -1,4 +1,4 @@
-package com.web.GuiaWeb;
+package com.web.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- guiaWeb/src/test/java/com/web/proyecto/GuiaWebApplicationTests.java
+++ guiaWeb/src/test/java/com/web/proyecto/GuiaWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.web.GuiaWeb;
+package com.web.proyecto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
